c73a68f548f84601523162dba6fe0e9d7abc7fe1
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-11 16:16:47 +0530

    Change for downloading the Spring Cloud modules

--- cloud-config-app/src/main/java/io/yadnyesh/cloudconfigapp/CloudConfigAppApplication.java
+++ cloud-config-app/src/main/java/io/yadnyesh/cloudconfigapp/CloudConfigAppApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudConfigAppApplication {
 
+public class CloudConfigAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigAppApplication.class, args);
 	}
