007a96e3c1b29f5e772f07ee0b5eb39d65df48d0
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-08-11 16:35:08 +0530

    Enabling Config server

--- cloud-config-app/src/main/java/io/yadnyesh/cloudconfigapp/CloudConfigAppApplication.java
+++ cloud-config-app/src/main/java/io/yadnyesh/cloudconfigapp/CloudConfigAppApplication.java
@@ -2,9 +2,10 @@ package io.yadnyesh.cloudconfigapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-
+@EnableConfigServer
 public class CloudConfigAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigAppApplication.class, args);
